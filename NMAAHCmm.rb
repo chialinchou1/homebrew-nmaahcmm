3d3cb36f346d97f687c4717227577f313d614844
BleakleyMcD <bleakleym@gmail.com>
2018-12-13 18:38:05 -0500

    changes

--- NMAAHCmm.rb
+++ NMAAHCmm.rb
@@ -1,22 +1,22 @@
 class Nmaahcmm < Formula
   desc "scripts for running NMAAHC microservices"
-  homepage "https://github.com/NMAAHC/NMAAHCmm"
-  url "https://github.com/NMAAHC/NMAAHCmm/archive/v0.0.1.tar.gz"
+  homepage "https://github.com/NMAAHC/nmaahcmm"
+  url "https://github.com/NMAAHC/nmaahcmm/archive/v0.0.1.tar.gz"
   sha256 "e281a6d80ffaedbab929e5eaeaed4accdf0ad263c0df39d96e1bfb26cbace0cc"
-  head "https://github.com/NMAAHC/NMAAHCmm.git"
+  head "https://github.com/NMAAHC/nmaahcmm.git"
 
   depends_on "cowsay"
-  depends_on "dvdrtools"
   depends_on "dvdauthor"
+  depends_on "dvdrtools"
   depends_on "exiftool"
-  depends_on "sdl"
   depends_on "ffmpeg" => ["with-sdl2", "with-freetype"]
   depends_on "flac"
   depends_on "md5deep"
-  depends_on "mediaconch"
   depends_on "media-info"
+  depends_on "mediaconch"
   depends_on "normalize"
   depends_on "rsync"
+  depends_on "sdl"
   depends_on "tree"
   depends_on "xmlstarlet"
 
